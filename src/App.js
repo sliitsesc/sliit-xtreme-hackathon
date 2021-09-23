da26df00149c18ece79cc8b436ec05f365f7078b
unknown <chenukasumanasekara@gmail.com>
2021-09-24 01:56:15 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import { scores } from './data/scores';
 
 function App() {
+
+  let data = scores.sort((a, b) => {
+    return b.score - a.score
+  })
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Heading-container">
+          <div className={"animate__animated animate__pulse animate__infinite"}><h1 className={"Heading"}>LEADERBOARD</h1></div>
+          <div className="Table-container">
+          <table class="table table-striped table-condensed table-bordered">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Team Name</th>
+      <th scope="col">Score</th>
+    </tr>
+  </thead>
+  <tbody>
+    {
+      data ? (
+        data.map((e, key) => (
+          <tr key={key}>
+            <th scope="row">{key + 1}</th>
+            <td>{e.teamName}</td>
+            <td>{e.score}</td>
+          </tr>
+        ))
+      ) : (<tr>Waiting for the results</tr>)
+    }
+  </tbody>
+</table>
+          </div>
+        </div>
       </header>
     </div>
   );
---
+++ src/data/scores.js
@@ -0,0 +1,22 @@
+export const scores = [
+    /* {
+        teamName: "A",
+        score: 23
+    },
+    {
+        teamName: "B",
+        score: 57
+    },
+    {
+        teamName: "C",
+        score: 45
+    },
+    {
+        teamName: "D",
+        score: 32
+    },
+    {
+        teamName: "E",
+        score: 25
+    }, */
+]
